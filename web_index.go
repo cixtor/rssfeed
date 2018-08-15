1f554274e06903ae4a0786dfb10e35c3b5ece384
Cixtor <cixtords@gmail.com>
2018-08-15 09:00:00 -0800

    Add HTTP index endpoint to welcome the user to the application

---
+++ web_index.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func init() {
+	router.GET("/", webIndex)
+}
+
+func webIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!\n"))
+}
