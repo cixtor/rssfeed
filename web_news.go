7da443cb358b3e5cf90135f4b35f8a4c226245bd
Cixtor <cixtords@gmail.com>
2018-08-16 09:00:00 -0800

    Add HTTP news.rss endpoint to fetch and curate the RSS feed

---
+++ web_news.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/cixtor/rssfeed/newsfeed"
+)
+
+const concurrency int = 30
+
+func init() {
+	router.GET("/news.rss", webNews)
+}
+
+func webNews(w http.ResponseWriter, r *http.Request) {
+	rss, err := newsfeed.New(concurrency)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	rss.DownloadItems()
+
+	w.Header().Set("content-type", "application/rss+xml")
+
+	if err := rss.Encode(w); err != nil {
+		log.Println(err)
+		return
+	}
+}
