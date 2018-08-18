0bcf0bfef64848ac527ec00057899862d7931e82
Cixtor <cixtords@gmail.com>
2018-08-18 09:00:00 -0800

    Add content-type white-list to avoid unnecessary binary parsing

---
+++ newsfeed/ctype.go
@@ -0,0 +1,24 @@
+package newsfeed
+
+import (
+	"strings"
+)
+
+func isValidContentType(ctype string) bool {
+	allowed := map[string]bool{
+		"text/html":                     true,
+		"application/rss+xml":           true,
+		"text/html;charset=utf-8":       true,
+		"text/html; charset=utf-8":      true,
+		"text/html; charset=iso-8859-1": true,
+		"text/html; charset=\"UTF-8\"":  true,
+	}
+
+	ctype = strings.ToLower(ctype)
+
+	if _, ok := allowed[ctype]; ok {
+		return true
+	}
+
+	return false
+}
