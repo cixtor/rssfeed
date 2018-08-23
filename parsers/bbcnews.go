d2c9cdda4117a9df7c81b23eb04759479d1640ec
Cixtor <cixtords@gmail.com>
2018-08-23 09:00:00 -0800

    Add parser to curate articles published on www.bbc.co.uk

---
+++ parsers/bbcnews.go
@@ -0,0 +1,20 @@
+package parsers
+
+import (
+	"strings"
+)
+
+func init() {
+	register("www.bbc.co.uk", bbcnews)
+}
+
+func bbcnews(body string) string {
+	var mark int
+
+	mark = strings.Index(body, `<div class="story-body__inner" property="articleBody">`)
+	body = body[mark:]
+	mark = strings.Index(body, `<div id="topic-tags">`)
+	body = body[0:mark]
+
+	return body
+}
