a9ce3f0a275211253755e6e411ac3fe2e1cdc4a2
Cixtor <cixtords@gmail.com>
2018-09-26 09:00:00 -0800

    Fix content parser for articles posted on www.bloomberg.com

--- parsers/bloomberg.go
+++ parsers/bloomberg.go
@@ -12,8 +12,13 @@ func bloomberg(body string) string {
 	var mark int
 
 	mark = strings.Index(body, `<div class="body-copy fence-body">`)
-	body = body[mark+34:]
-	mark = strings.Index(body, `<ol class="noscript-footnotes">`)
+
+	if mark == -1 {
+		mark = strings.Index(body, `<div class="body-copy-v2 fence-body">`)
+	}
+
+	body = body[mark:]
+	mark = strings.Index(body, `<ol class="noscript-footnotes"></ol>`)
 	body = body[0:mark]
 
 	return body
