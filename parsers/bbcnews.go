7d96b0fea91c3bc48b804762d379177f285da241
Cixtor <cixtords@gmail.com>
2019-01-09 09:00:00 -0800

    Fix invalid index array references for nytimes and bbcnews

--- parsers/bbcnews.go
+++ parsers/bbcnews.go
@@ -12,6 +12,11 @@ func bbcnews(body string) string {
 	var mark int
 
 	mark = strings.Index(body, `<div class="story-body__inner" property="articleBody">`)
+
+	if mark == -1 {
+		return body
+	}
+
 	body = body[mark:]
 	mark = strings.Index(body, `<div id="topic-tags">`)
 	body = body[0:mark]
--- parsers/nytimes.go
+++ parsers/nytimes.go
@@ -12,6 +12,11 @@ func nytimes(body string) string {
 	var mark int
 
 	mark = strings.Index(body, `<article`)
+
+	if mark == -1 {
+		return body
+	}
+
 	body = body[mark:]
 	mark = strings.Index(body, `</article>`)
 	body = body[0:mark]
